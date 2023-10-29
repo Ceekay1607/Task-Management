5c60c4837b5b02a6c0301796bde4564bd79867f1
Do Ly Anh Thu <anhthu270302@gmail.com>
2023-10-29 11:15:21 +0700

    add data of priority and category

--- backend/seeds/seed.js
+++ backend/seeds/seed.js
@@ -5,11 +5,49 @@ function createFakeUser() {
         name: faker.person.fullName(),
         email: faker.internet.email(),
         image: faker.image.avatar(),
-        color: faker.internet.color(),
     };
 }
+
+const category = [
+    {
+        type: "TODO",
+        name: "To Do",
+        order: 0,
+    },
+
+    {
+        type: "ONPROGRESS",
+        name: "On Progress",
+        order: 1,
+    },
+
+    {
+        type: "DONE",
+        name: "Done",
+        order: 2,
+    },
+];
+
+const priority = [
+    {
+        name: "Low",
+        order: 0,
+    },
+
+    {
+        name: "Medium",
+        order: 1,
+    },
+
+    {
+        name: "High",
+        order: 2,
+    },
+];
 
 exports.seed = async function (knex) {
     await knex("user").del();
     await knex("user").insert(Array(25).fill().map(createFakeUser));
+    await knex("category").insert(category);
+    await knex("priority").insert(priority);
 };
