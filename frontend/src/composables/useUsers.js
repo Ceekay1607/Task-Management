3b5fc822ca8c4e0730f60ecaf545360ffbe43d98
Ngu Cong Khanh <88470967+Ceekay1607@users.noreply.github.com>
2023-11-05 09:12:12 +0700

    create view for login

--- frontend/src/composables/useUsers.js
+++ frontend/src/composables/useUsers.js
@@ -1,22 +1,30 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/vue-query";
 import userService from "../service/users.service";
+import { computed } from "vue";
 
 export function useUsers() {
     const queryClient = useQueryClient();
 
     function retrieveUsersQuery() {
-        const { data: users, ...rest } = useQuery({
+        const { data: usersArray } = useQuery({
             queryKey: ["users"],
             queryFn: () => userService.getUsers(),
         });
 
+        const users = computed(() => {
+            const tempUser = usersArray.value?.users.map((user) => user) ?? [];
+
+            return tempUser.sort((current, next) =>
+                current.name.localeCompare(next.name)
+            );
+        });
+
         return {
             users,
-            ...rest,
         };
     }
 
     return {
-        retrieveUsers: retrieveUsersQuery,
+        retrieveUsersQuery,
     };
 }
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,10 +1,55 @@
+<template>
+    <div
+        class="container d-flex flex-column justify-content-center align-items-center min-vh-100"
+    >
+        <div class="w-50 text-center">
+            <h1 class="font-weight-bold text-center mb-4">Select login user</h1>
+            <form @submit.prevent="handleSubmit" class="text-center">
+                <div class="form-group">
+                    <select
+                        v-model="selectedValue"
+                        class="form-control w-50 mx-auto"
+                        @change="onValueChange"
+                        id="userSelect"
+                    >
+                        <option value="" disabled>Select user</option>
+                        <option
+                            v-for="user in users"
+                            :key="user.id"
+                            :value="user"
+                            class="d-flex align-items-center"
+                        >
+                            {{ user.name }}
+                        </option>
+                    </select>
+                </div>
+                <button
+                    type="submit"
+                    name="_action"
+                    value="setUser"
+                    class="btn btn-primary btn-block mt-3 w-50 mx-auto"
+                >
+                    Login
+                </button>
+            </form>
+        </div>
+    </div>
+</template>
+
 <script setup>
+import { ref } from "vue";
 import { useUsers } from "../composables/useUsers";
 
-const { retrieveUsers } = useUsers();
-const users = retrieveUsers();
-</script>
+const { retrieveUsersQuery } = useUsers();
+const { users } = retrieveUsersQuery();
 
-<template>
-    <h1>Hi, this is Login page</h1>
-</template>
+const selectedUser = ref(null);
+const selectedValue = ref(null);
+
+const onValueChange = () => {
+    console.log("Selected User changed:", selectedValue.value.id);
+    selectedUser.value = selectedValue.value;
+};
+
+const handleSubmit = () => {};
+</script>
