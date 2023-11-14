32d67bf62eb1cf8d5b788853193047d533066dda
Ngu Cong Khanh <88470967+Ceekay1607@users.noreply.github.com>
2023-11-15 01:27:58 +0700

    get issues by tanstack

---
+++ frontend/src/components/issues/IssueBoard.vue
@@ -0,0 +1,14 @@
+<template>board</template>
+
+<script setup>
+import { useIssues } from "@/composables/useIssues";
+import { useRoute } from "vue-router";
+import { ref } from "vue";
+
+const route = useRoute();
+const projectId = ref(route.params.projectId);
+
+const { retrieveIssues } = useIssues();
+const { issues } = retrieveIssues(projectId);
+console.log(issues);
+</script>
